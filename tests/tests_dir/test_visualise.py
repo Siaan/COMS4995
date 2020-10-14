79e91789d2c6ca68deae2d63ca38dae38b618fdc
Siaan <sianleekitt@yahoo.com>
2020-10-13 20:29:25 -0400

    moved scripts into tests

--- tests/tests_dir/test_visualise.py
+++ tests/tests_dir/test_visualise.py
@@ -1,16 +1,16 @@
-from scripts.main import visualise 
+from tests.scripts.main import visualise 
 from unittest import mock
-import scripts.main.clean_KF as KF
+#import scripts.main.clean_KF as KF
 
 import numpy as np
 
 
-@mock.patch("%s.KF.plt" % __name__)
+@mock.patch("%s.visualise.plt" % __name__)
 def test_visualise(mock_plt):
     x = np.arange(0,5,0.1)
     y = np.sin(x)
     x_messy = np.arange(0,5,0.1)
-    KF.visualise(x,y, x_messy)
+    visualise(x,y, x_messy)
 
     mock_plt.title.assert_called_once_with("RTS Smoother")
     assert mock_plt.figure.called
